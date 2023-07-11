c44816e8ca0a86bfd57900357f1993061e793e5b
wangyunfei <wangyunfei@cdsz.net.com>
2023-07-11 10:11:34 +0800

    docker build config

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build --build-arg AUTHORS=wangyunfei -t base-wendriver:1.0.0-SNAPSHOT .
---
+++ dev-deploy.sh
@@ -0,0 +1,4 @@
+yum install firefox
+firefox.x86_64 0:102.12.0-1.el7.centos
+geckodriver-v0.33.0-linux64.tar.gz
+chomd 777 geckodriver
